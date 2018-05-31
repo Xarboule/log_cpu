7f9a35ba9ef37df98766480c1f6306573789aa95
Rémi <remi.dulong@telecom-sudparis.eu>
2018-05-31 14:51:26 +0200

    Xeon mode with 2 sockets

--- log_cpu_freq.c
+++ log_cpu_freq.c
@@ -14,7 +14,7 @@
 
 #include "msr_utils.h"
 
-//define XEON
+#define XEON // for nova servers
 
 extern int errno;
 
@@ -36,7 +36,8 @@ void write_msr_config_xeon(){
 		exit(EXIT_FAILURE);
 		
 	}
-	write_msr(0,0x703,1<<22);
+	write_msr(0,0x703,1<<22); //First socket
+	write_msr(9,0x703,1<<22); //Second socket
 }
 
 
@@ -50,7 +51,9 @@ int main(int argc, char * argv[]){
 
 	const int nproc = get_nprocs();
 	printf("Monitoring %d CPUs.\n", nproc);
-
+#ifdef XEON
+	printf("XEON MODE WITH 2 SOCKETS\n");
+#endif
 	printf("Initializing MSR for uncore frequency reading...\n");
 
 #ifdef XEON
@@ -137,20 +140,33 @@ int main(int argc, char * argv[]){
 		int cur_measure;
 
 		uint64_t uncore_freq;
-
-		
 		uint64_t previous_uncore_clk, cur_uncore_clk;
+#ifdef XEON
+		uint64_t uncore_freq_1;
+		uint64_t previous_uncore_clk_1, cur_uncore_clk_1;
+#endif
+		
 		struct timespec res1, res2;
 		while(42){ // Periodic measures
 			sleep(1);
 			res2.tv_sec = 0;
 			clock_gettime(CLOCK_MONOTONIC, &res1);
 			previous_uncore_clk = read_msr(0, addr_msr_read);
+#ifdef XEON
+			previous_uncore_clk_1 = read_msr(8, addr_msr_read);
+#endif
 			usleep(1000);	
 			cur_uncore_clk = read_msr(0, addr_msr_read);
+#ifdef XEON
+			cur_uncore_clk = read_msr(8, addr_msr_read);
+#endif
 			clock_gettime(CLOCK_MONOTONIC, &res2);
 			previous_uncore_clk &= ((1uL<<48)-1);
 		       	cur_uncore_clk &= ((1uL<<48)-1);
+#ifdef XEON
+			previous_uncore_clk_1 &= ((1uL<<48)-1);
+		       	cur_uncore_clk_1 &= ((1uL<<48)-1);
+#endif						
 			
 			char result[300] = "";
 			printf("Measure !\n");
@@ -159,13 +175,23 @@ int main(int argc, char * argv[]){
 			uncore_freq = (cur_uncore_clk - previous_uncore_clk)/elapsed; // KHz
 			printf("Uncore Frequency: %li\n", uncore_freq);
 			previous_uncore_clk = cur_uncore_clk;
+#ifdef XEON
+			uncore_freq_1 = (cur_uncore_clk_1 - previous_uncore_clk_1)/elapsed; // KHz
+			printf("Uncore Frequency 1 : %li\n", uncore_freq_1);
+			previous_uncore_clk_1 = cur_uncore_clk_1;
 			
+#endif
 			char timestamp[20] = "";
 			sprintf(timestamp, "%lu, ", (unsigned long)time(NULL));
 			strcat(result, timestamp);	
 			char uncore_result[20] = "";
 			sprintf(uncore_result, "%li, ", uncore_freq);
 			strcat(result, uncore_result);
+#ifdef XEON
+			char uncore_result_1[20] = "";
+			sprintf(uncore_result_1, "%li, ", uncore_freq_1);
+			strcat(result, uncore_result_1);
+#endif
 
 			for(int i=0; i<nproc; i++){
 				char atomic_result[10] = "";
